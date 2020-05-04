8c6ae7bd4e634377a0dcb427aac44411168b7a8b
Bradey Whitlock <bradeyywhitlock@gmail.com>
2020-05-04 08:40:22 -0500

    Adding stream is live button and twitch service to check dumbdog stream and day if friday to show button or not

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -2,21 +2,24 @@ import React, { ReactElement, useEffect, useState } from 'react';
 import TCLLogo from '../../assets/images/tclLogo.png'
 import moment from 'moment'
 import './styles/home.scss'
+import {isTclLive} from '../../services/TwitchService'
 
 const Home = (): ReactElement => {
     const isMobile = window.innerWidth <= 500
     const streamDay = moment().day(5).hour(13).minutes(30);
     const now = new Date()
     const [duration, setDuration] = useState(moment.duration(streamDay.diff(now)))
-
+    const [tclLive, setTclLive] = useState(false)
 
     useEffect(() => {
+        isTclLive().then(res => {setTclLive(res)})
         setInterval(() => {
             const now = new Date()
             const streamDay = moment().day(5).hour(13).minutes(30).utcOffset(-6);
             var duration = moment.duration(streamDay.diff(now));
             setDuration(duration)
-        }, 1000)
+            isTclLive().then(res => {setTclLive(res)})
+        }, 20000)
     }, [])
 
     return (
@@ -24,10 +27,9 @@ const Home = (): ReactElement => {
             <div className='homeLogo'>
                 <img src={TCLLogo} width={515} height={217}/>
             </div>
+            {!tclLive ?
             <div className='nextStream'>Next Stream: {duration.days()} Days • {duration.hours()} Hours • {duration.minutes()} Mins</div>
-            {/* <div className='homeDescription'>
-                Small description about the checkpoint league.
-            </div> */}
+            : <a target='_blank' href='https://www.twitch.tv/dumbdog' className='liveButton'>Currently Live!</a>}
             <iframe className='trailerVideo' width={isMobile ? "360" : "1120"} height={isMobile ? "200" : "630"} src='https://www.youtube.com/embed/CChwLKLT-dA' ></iframe>
         </div>
     )
---
+++ src/services/TwitchService.ts
@@ -0,0 +1,17 @@
+import axios from 'axios'
+import * as Config from '../config';
+import moment from 'moment';
+
+export const isTclLive = async (): Promise<boolean> => {
+    var tclLive = await axios.get(`https://api.twitch.tv/helix/streams?user_login=dumbdog`,
+    {headers: {"Client-ID": Config.twitterAuth}})
+    .then(res => {
+        if(res.data.data.length !== 0 && moment().day() === 5) {
+            return true
+        } else {
+            return false
+        }
+    }).catch(() => {return false})
+
+    return tclLive
+}
